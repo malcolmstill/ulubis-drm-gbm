9cd5bcdd8b0507dec67f5d17dc87ab5c64a4dbd2
Malcolm Still <malcolm.still@gmail.com>
2018-07-24 21:59:25 +0100

    EGL support

--- ulubis-drm-gbm.lisp
+++ ulubis-drm-gbm.lisp
@@ -120,6 +120,49 @@
   (syscall:ioctl (tty-fd backend) +KDSETMODE+ +KD-TEXT+)
   (nix:close (tty-fd backend)))
 
+(defmethod ulubis-backend:init-egl ((backend backend) wl-display)
+  (egl:init-egl-wayland)
+  (egl:bind-wayland-display (cepl.drm-gbm::get-egl-display) wl-display))
+
+(defmethod ulubis-backend:egl-supported? ((backend backend))
+  t)
+
+(defmethod ulubis-backend:egl-surface? ((backend backend) buffer)
+  (let ((display (cepl.drm-gbm::get-egl-display)))
+    (with-foreign-object (texture-format :uint32)
+      (not (= 0 (egl:query-wayland-buffer display buffer #x3080 texture-format))))))
+
+(defmethod ulubis-backend:egl-get-dimensions ((backend backend) buffer)
+  (let ((display (cepl.drm-gbm::get-egl-display)))
+    (with-foreign-objects ((width :uint32) (height :uint32) (attribs :uint32))
+      (egl:query-wayland-buffer display buffer #x3057 width)
+      (egl:query-wayland-buffer display buffer #x3056 height)
+      (values (mem-aref width :uint32)
+	      (mem-aref height :uint32)))))
+
+(defmethod ulubis-backend:egl-texture-from-image ((backend backend) buffer width height)
+  (with-foreign-objects ((attribs :uint32))
+    (setf (mem-ref attribs :int32) #x3038)
+    (let* ((display (cepl.drm-gbm::get-egl-display))
+	   (context (cepl.drm-gbm::get-egl-context))
+	   (image (egl:create-image display context #x31D5 buffer attribs))
+	   (texture (make-egl-texture image width height)))
+      (egl:destroy-image display image)
+      texture)))
+
+(defun make-egl-texture (image width height)
+  (let* ((ids (gl:gen-textures 1))
+	 (id (first ids)))
+    (gl:bind-texture :texture-2d id)
+    (gl:tex-parameter :texture-2d :texture-mag-filter :nearest)
+    (gl:tex-parameter :texture-2d :texture-min-filter :nearest)
+    (gl:tex-parameter :texture-2d :texture-max-level 0)
+    (egl:image-target-texture-2DOES #xDE1 image)
+    (gl:bind-texture :texture-2d 0)
+    (cepl:make-texture-from-id id
+			       :element-type :rgba8
+			       :base-dimensions (list width height))))
+
 (defun make-drm-event-context (vblank-callback page-flip-callback)
   (let ((context (foreign-alloc '(:struct drm:event-context))))
     (with-foreign-slots ((drm:version drm:vblank-handler drm:page-flip-handler) context (:struct drm:event-context))
